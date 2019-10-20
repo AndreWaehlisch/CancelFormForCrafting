193cc79ee6f5884c01f1bd2bfd80ca5ad6b7021e
André Wählisch <AndreWaehlisch@users.noreply.github.com>
2019-10-20 02:03:15 +0200

    re-release for WoW classic

--- CancelFormForCraftingCore.lua
+++ CancelFormForCraftingCore.lua
@@ -1,6 +1,6 @@
 --only enable when player is druid
 if ((select(2,UnitClass("player"))) ~= "DRUID") then
-	return
+	return;
 end;
 
 --setup locals
@@ -13,16 +13,16 @@ local function ReplaceButton(oldbuttonLink)
 	--steal position and script handler from the old button
 	newbuttonLink:SetAllPoints(oldbuttonLink);
 	newbuttonLink:HookScript("OnClick",oldbuttonLink:GetScript("OnClick"));
-	
+
 	--always hide old button
 	oldbuttonLink:HookScript("OnShow",function()
-		if InCombatLockdown() or userCombat then
+		if ( InCombatLockdown() or userCombat ) then
 			return;
 		end;
-		
+
 		oldbuttonLink:Hide();
 		newbuttonLink:Show();
-		
+
 		if newbuttonLink:IsVisible() then
 			if ( newbuttonLink:GetFrameLevel() <= TradeSkillFrame:GetFrameLevel() ) then
 				newbuttonLink:SetFrameLevel(TradeSkillFrame:GetFrameLevel()+1);
@@ -31,102 +31,158 @@ local function ReplaceButton(oldbuttonLink)
 	end);
 
 	--set attributes to run a macro which does our protected stuff
-	newbuttonLink:SetAttribute("type","macro");
-	newbuttonLink:SetAttribute("macrotext",SLASH_CANCELFORM1);
+	newbuttonLink:SetAttribute("type", "macro");
+	newbuttonLink:SetAttribute("macrotext", SLASH_CANCELFORM1);
 
 	return newbuttonLink;
 end;
 
 --do our stuff when user opens trade skill window for first time
 local ProfessionOpenedFrame = CreateFrame("Frame");
+ProfessionOpenedFrame:RegisterEvent("CRAFT_SHOW");
 ProfessionOpenedFrame:RegisterEvent("TRADE_SKILL_SHOW");
 ProfessionOpenedFrame:SetScript("OnEvent",function()
 	--initialize when not in combat and trade skill window
-	if ( not IsTradeSkillLinked() ) and ( not IsTradeSkillGuild() ) and ( TradeSkillCreateButton ) and ( TradeSkillCreateAllButton ) and ( not InCombatLockdown() ) then
+	if ( CraftCreateButton or TradeSkillCreateButton ) and ( not InCombatLockdown() ) then
 		--save old buttons
-		local OldOneButton, OldAllButton = TradeSkillCreateButton, TradeSkillCreateAllButton;
+		local OldOneButton_craft = CraftCreateButton; -- for e.g. enchanting
+		local OldOneButton, OldAllButton = TradeSkillCreateButton, TradeSkillCreateAllButton; -- for e.g. cooking, alchemy...
 
-		--create new buttons
-		local NewOneButton = ReplaceButton(OldOneButton);
-		local NewAllButton = ReplaceButton(OldAllButton);
+		if ( OldOneButton_craft ) then
+			-- create new button
+			local NewOneButton_craft = ReplaceButton(OldOneButton_craft);
 
-		--set label of AllButton (label of OneButton is set on-the-fly)
-		NewAllButton:SetText(CREATE_ALL);
-		
-		--enable/disable and show/hide our buttons
-		hooksecurefunc("TradeSkillFrame_SetSelection",function(id)
-			if InCombatLockdown() or userCombat then
-				return;
+			--enable/disable and show/hide our buttons
+			hooksecurefunc("CraftFrame_SetSelection", function(id)
+				if ( InCombatLockdown() or userCombat ) then
+					return;
+				end;
+
+				OldOneButton_craft:Hide();
+
+				if ( select(4, GetCraftInfo(id)) ) > 0 then
+					NewOneButton_craft:Enable();
+				else
+					NewOneButton_craft:Disable();
+				end;
+
+				NewOneButton_craft:SetText(_G[GetCraftButtonToken()]);
+				NewOneButton_craft:Show();
+
+				if NewOneButton_craft:IsVisible() then
+					if ( NewOneButton_craft:GetFrameLevel() <= CraftFrame:GetFrameLevel() ) then
+						NewOneButton_craft:SetFrameLevel(CraftFrame:GetFrameLevel()+1);
+					end;
+				end;
+			end);
+
+			--on setup we have to run code above or click on Create(All)Button will not work until we change selection
+			local tsIndex = GetCraftSelectionIndex();
+			if ( tsIndex == 0 ) then
+				tsIndex = GetFirstTradeSkill();
 			end;
-			
-			OldOneButton:Hide();
-			OldAllButton:Hide();
-			
-			if (select(3,GetTradeSkillInfo(id))) > 0 then
-				NewOneButton:Enable();
-				NewAllButton:Enable();
-			else
-				NewOneButton:Disable();
-				NewAllButton:Disable();
-			end
-			
-			if ( IsTradeSkillGuild() ) or ( IsTradeSkillLinked() ) then
-				NewOneButton:Hide();
-				NewAllButton:Hide();
-			else
-				local altverb = ( select(5,GetTradeSkillInfo(id)) );
+			CraftFrame_SetSelection(tsIndex);
+
+			--when we enter combat our addon is disabled so we dont cause taint: show old button
+			local CombatFrame = CreateFrame("Frame");
+			CombatFrame:RegisterEvent("PLAYER_REGEN_DISABLED");
+			CombatFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
+			CombatFrame:SetScript("OnEvent",function(self,event)
+				if event == "PLAYER_REGEN_DISABLED" then
+					userCombat = true;
+					NewOneButton_craft:Hide();
+					OldOneButton_craft:Show();
+				else
+					userCombat = false;
+					local tsIndex = GetCraftSelectionIndex();
+					if ( tsIndex == 0 ) then
+						tsIndex = GetFirstTradeSkill();
+					end;
+					CraftFrame_SetSelection(tsIndex);
+				end;
+			end);
+
+			ProfessionOpenedFrame:UnregisterEvent("CRAFT_SHOW");
+		end;
+
+		if ( TradeSkillCreateButton ) then
+			--create new buttons
+			local NewOneButton = ReplaceButton(OldOneButton);
+			local NewAllButton = ReplaceButton(OldAllButton);
+
+			--set label of AllButton (label of OneButton is set on-the-fly)
+			NewAllButton:SetText(CREATE_ALL);
+
+			--enable/disable and show/hide our buttons
+			hooksecurefunc("TradeSkillFrame_SetSelection",function(id)
+				if ( InCombatLockdown() or userCombat ) then
+					return;
+				end;
+
+				OldOneButton:Hide();
+				OldAllButton:Hide();
+
+				if ( select(3, GetTradeSkillInfo(id)) > 0 ) then
+					NewOneButton:Enable();
+					NewAllButton:Enable();
+				else
+					NewOneButton:Disable();
+					NewAllButton:Disable();
+				end;
+
+				local altverb = ( select(5, GetTradeSkillInfo(id)) );
 				if altverb then
 					NewAllButton:Hide();
 				else
 					NewAllButton:Show();
 				end;
-				
+
 				NewOneButton:SetText(altverb or CREATE_PROFESSION);
 				NewOneButton:Show();
-			end;
-			
-			if NewOneButton:IsVisible() then
-				if ( NewOneButton:GetFrameLevel() <= TradeSkillFrame:GetFrameLevel() ) then
-					NewOneButton:SetFrameLevel(TradeSkillFrame:GetFrameLevel()+1);
+
+				if NewOneButton:IsVisible() then
+					if ( NewOneButton:GetFrameLevel() <= TradeSkillFrame:GetFrameLevel() ) then
+						NewOneButton:SetFrameLevel(TradeSkillFrame:GetFrameLevel()+1);
+					end;
 				end;
-			end;
-			
-			if NewAllButton:IsVisible() then
-				if ( NewAllButton:GetFrameLevel() <= TradeSkillFrame:GetFrameLevel() ) then
-					NewAllButton:SetFrameLevel(TradeSkillFrame:GetFrameLevel()+1);
+
+				if NewAllButton:IsVisible() then
+					if ( NewAllButton:GetFrameLevel() <= TradeSkillFrame:GetFrameLevel() ) then
+						NewAllButton:SetFrameLevel(TradeSkillFrame:GetFrameLevel()+1);
+					end;
 				end;
+			end);
+
+			--on setup we have to run code above or click on Create(All)Button will not work until we change selection
+			local tsIndex = GetTradeSkillSelectionIndex();
+			if ( tsIndex == 0 ) then
+				tsIndex = GetFirstTradeSkill();
 			end;
-		end);
-		
-		--on setup we have to run code above or click on Create(All)Button will not work until we change selection
-		local tsIndex = GetTradeSkillSelectionIndex();
-		if ( tsIndex == 0 ) then
-			tsIndex = GetFirstTradeSkill();
-		end;
-		TradeSkillFrame_SetSelection(tsIndex);
-		
-		--when we enter combat our addon is disabled so we dont cause taint: show old buttons
-		local CombatFrame = CreateFrame("Frame");
-		CombatFrame:RegisterEvent("PLAYER_REGEN_DISABLED");
-		CombatFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
-		CombatFrame:SetScript("OnEvent",function(self,event)
-			if event == "PLAYER_REGEN_DISABLED" then
-				userCombat = true;
-				NewOneButton:Hide();
-				NewAllButton:Hide();
-				OldOneButton:Show();
-				if ( select(5,GetTradeSkillInfo(TradeSkillFrame.selectedSkill)) ) then
-					OldAllButton:Hide();
+			TradeSkillFrame_SetSelection(tsIndex);
+
+			--when we enter combat our addon is disabled so we dont cause taint: show old buttons
+			local CombatFrame = CreateFrame("Frame");
+			CombatFrame:RegisterEvent("PLAYER_REGEN_DISABLED");
+			CombatFrame:RegisterEvent("PLAYER_REGEN_ENABLED");
+			CombatFrame:SetScript("OnEvent",function(self,event)
+				if event == "PLAYER_REGEN_DISABLED" then
+					userCombat = true;
+					NewOneButton:Hide();
+					NewAllButton:Hide();
+					OldOneButton:Show();
+					if ( select(5, GetTradeSkillInfo(TradeSkillFrame.selectedSkill)) ) then
+						OldAllButton:Hide();
+					else
+						OldAllButton:Show();
+					end;
 				else
-					OldAllButton:Show();
+					userCombat = false;
+					TradeSkillFrame_SetSelection(TradeSkillFrame.selectedSkill);
 				end;
-			else
-				userCombat = false;
-				TradeSkillFrame_SetSelection(TradeSkillFrame.selectedSkill);
-			end;
-		end);
-		
-		ProfessionOpenedFrame:UnregisterAllEvents();
+			end);
+
+			ProfessionOpenedFrame:UnregisterEvent("TRADE_SKILL_SHOW");
+		end;
 	elseif InCombatLockdown() then
 		if ( GetLocale() == "deDE" ) then
 			print("CancelFormForCrafting: Konnte das Addon nicht aktivieren, weil du im Kampf bist. Um das Addon zu aktivieren, öffne ein Beruf-Fenster, sobald du nicht mehr im Kampf bist.");
